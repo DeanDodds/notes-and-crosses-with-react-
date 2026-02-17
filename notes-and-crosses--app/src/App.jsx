15aac0e7c4d6b6d6baad3f58ea1310ca1bd86aaa
DeanDodds <92326699+DeanDodds@users.noreply.github.com>
2026-02-17 20:56:34 +0000

    Winning functionality added

--- notes-and-crosses--app/src/App.jsx
+++ notes-and-crosses--app/src/App.jsx
@@ -4,23 +4,67 @@ import Header from './components/Header/Header'
 import Player from './components/Player/Player'
 import GameBoard from './components/GameBoard/GameBoard'
 import Logs from './components/Logs/Logs'
+import { WINNING_COMBINATIONS } from './winning-combonations'
+
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+
 
 function App() {
-  const [gameState, setGameState] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
-  const [gameTurns, setGameTurns] = useState([]); // Added gameTurns state
+
+  function getActivePlayer(gameTurns) {
+    let currentPlayer = "X";
+      if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
+        currentPlayer = "O";
+      }
+      return currentPlayer;
+  }
 
   function handlePlayerChange(rowIndex, colIndex) {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === "X" ? "O" : "X");
-
-    setGameTurns((prevTurns) => {
+    setGameTurns((gameTurns) => {
+      const currentPlayer = getActivePlayer(gameTurns);
+  
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
-        ...prevTurns,
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...gameTurns,
       ];
       return updatedTurns;
     });
   }
+
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = getActivePlayer(gameTurns);
+  
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const {square, player } = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+}
+
+    let winner = null;
+
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstPlayerSymbol = gameBoard[combination[0].row][combination[0].column];
+      const secondPlayerSymbol = gameBoard[combination[1].row][combination[1].column];
+      const thirdPlayerSymbol = gameBoard[combination[2].row][combination[2].column];
+
+      if (firstPlayerSymbol && 
+        firstPlayerSymbol === secondPlayerSymbol && 
+        firstPlayerSymbol === thirdPlayerSymbol
+      ) {
+        winner = firstPlayerSymbol;
+      }
+    }
+  
 
   return (
     <>
@@ -32,9 +76,9 @@ function App() {
         <Player initialName="Player 1" playerSymbol="X" isActive={activePlayer === 'X'}/>
         <Player initialName="Player 2" playerSymbol="O" isActive={activePlayer === 'O'}/>
        </ol>
-        
-       <GameBoard onPlayerChange={handlePlayerChange} turns={gameTurns}/>
-    </div>
+        {winner && <p>You Won {winner}</p>}
+       <GameBoard onPlayerChange={handlePlayerChange} board={gameBoard}/>
+        </div>
         <Logs turns={gameTurns} /> 
    </main>
    </>
--- notes-and-crosses--app/src/components/GameBoard/GameBoard.jsx
+++ notes-and-crosses--app/src/components/GameBoard/GameBoard.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-export default function GameBoard({ onPlayerChange, turns}) {
-//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const {square, player } = turn;
-    const {row, col} = square;
-
-    gameBoard[row][col] = player;
-}
-
-
+export default function GameBoard({ onPlayerChange, board}) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li className="game-row" key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
